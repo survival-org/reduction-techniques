a62045b0390bfa2eba90d79ca33ad440528bb98f
Wiegrebe <Simon.Wiegrebe@campus.lmu.de>
2024-12-12 14:56:11 +0100

    finished survival curves figure, except for pv

--- code/tumor_illustrations.r
+++ code/tumor_illustrations.r
@@ -8,9 +8,6 @@ library(pseudo)
 library(geepack)
 library(ggplot2)
 
-# helpers
-
-
 # load data
 data("tumor", package = "pammtools")
 
@@ -18,7 +15,7 @@ data("tumor", package = "pammtools")
 cutpoints <- seq(0, 3800, by=100)
 tumor_partitioned <- tumor %>% as_ped(Surv(days, status) ~ ., cut = cutpoints)
 
-# modeling
+# modeling ----
 
 ## KM
 km <- survfit(Surv(days, status) ~ complications, data = tumor, se.fit = TRUE)
@@ -27,17 +24,6 @@ km <- survfit(Surv(days, status) ~ complications, data = tumor, se.fit = TRUE)
 cox <- coxph(
     formula = Surv(days, status) ~ strata(complications),
     data = tumor)
-
-cox_df <- basehaz(cox) %>%
-  group_by(strata) %>%
-  mutate(prob_km = exp(-hazard)) %>%
-  ungroup() %>%
-  rename(complications = strata)
-
-ggplot(base_df, aes(x = time, y = nelson_aalen, group = strata)) +
-  geom_stephazard(data=cox_df, aes(x=time, y=prob_km)) +
-  ylab(expression(hat(Lambda)(t))) + xlab("t") +
-  ggtitle("Nelson-Aalen estimate of the cumulative hazard")
 
 ## DT
 dt <- gam(
@@ -51,7 +37,7 @@ pam <- pamm(
   data = tumor_partitioned)
 
 ## PV
-potsurv <- pseudosurv(tumor$days, tumor$status, tmax = 1:3)
+potsurv <- pseudosurv(tumor$days, tumor$status, tmax = seq(1000, 3800, by=1000)) # can't we do the same seq(0, 3800, by=100) here?
 longpbc3 <- NULL
 for(it in 1:length(potsurv$time)){
     longpbc3 <- rbind(
@@ -73,10 +59,26 @@ pv <- geese(
     corstr="independence",
     family = gaussian(link = "identity"))
 
-# survival curves
+# survival curves ----
 new_data <- tumor_partitioned %>%
   make_newdata(tend = unique(tend), complications = unique(complications))
 
+## KM
+km_df <- broom::tidy(km) %>%
+  mutate(
+    model = "km",
+    complications = ifelse(strata=="complications=yes", "yes", "no")
+  ) %>%
+  select(tend=time, prob=estimate, km_low=conf.low, km_high=conf.high, complications, model)
+
+## Cox
+cox_df <- basehaz(cox) %>%
+  group_by(strata) %>%
+  mutate(prob_km = exp(-hazard)) %>%
+  ungroup() %>%
+  rename(complications = strata)
+
+## PAM
 pred_pam <- new_data %>%
     group_by(complications) %>%
     add_surv_prob(pam) %>%
@@ -84,6 +86,7 @@ pred_pam <- new_data %>%
     rename(prob_pam = surv_prob) %>%
     select(tend, complications, prob_pam)
 
+## DT
 pred_dt <- new_data %>%
     mutate(
         eta_dt = predict(dt, newdata = ., type = "link"),
@@ -94,6 +97,7 @@ pred_dt <- new_data %>%
     ungroup() %>%
     select(tend, complications, prob_dt)
 
+## combine results
 pred <- pred_pam %>%
     left_join(pred_dt, by = c("tend", "complications"))
 
@@ -105,88 +109,91 @@ pred_long <- pred %>%
     names_pattern = "prob_(.*)"
   )
 
-km_df <- broom::tidy(km)
-km_long <- km_df %>%
-  rename(
-    tend = time,
-    prob = estimate,
-    complications = strata
-  ) %>%
-  mutate(
-    model = "km"
+## plot
+gg_survCurves <- ggplot(pred_long, aes(x = tend, y = prob)) +
+  geom_line(aes(color = model, linetype = complications), linewidth = linewidth) +
+  geom_stephazard(data = km_df, aes(linetype = complications, color = "km"), linewidth = linewidth) +
+  geom_ribbon(data = km_df, aes(ymin = km_low, ymax = km_high, fill = complications), alpha = .3, color = NA) +
+  scale_color_manual(
+    name = "model",
+    values = c("pam" = "blue", "dt" = "red", "km" = "black"),
+    breaks = c("pam", "dt", "km"),
+    labels = c("PAM", "DT", "KM")
+  ) +
+  scale_fill_manual(values = c("no" = "darkgrey", "yes" = "darkgrey")) +
+  labs(
+    x = "Time",
+    y = "Survival Probability"
+  ) +
+  theme_minimal(base_size = label_size) +
+  theme(
+    axis.title = element_text(size = label_size),
+    axis.text = element_text(size = label_size),
+    legend.text = element_text(size = label_size),
+    legend.position = "right"
   )
 
-gg_survCurves <- ggplot(pred_long, aes(x = tend, y = prob, color = interaction(complications, model))) +
-  geom_line(linewidth = 1) +
-  geom_stephazard(data=cox_df, aes(x=time, y=prob_km)) +
-  labs(
-    title = "Survival Probabilities by Model and Complications",
-    x = "Time (tend)",
-    y = "Survival Probability",
-    color = "Model & Complications"
-  ) +
-  theme_minimal()
-
 gg_survCurves
+ggsave("figures/survival_curves.png", gg_survCurves, width = 10, height = 6, dpi = 300) # TBD: add PV (whole curves or only points?) as dark orange/brown
 
-# RMST
+# RMST ----
 
-  ## PEM / PAM
+## PEM / PAM
 
-  # calculate rmst (integrate step function)
-  rmst_pam <- pred_pam %>%
-    group_by(complications) %>%
-    summarise(rmst = sum(prob_pam * diff(c(0, tend))))
+# calculate rmst (integrate step function)
+rmst_pam <- pred_pam %>%
+  group_by(complications) %>%
+  summarise(rmst = sum(prob_pam * diff(c(0, tend))))
 
-  rmst_pam
+rmst_pam
 
-  # transform into wide format for plotting area between survival curves
-  pred_pam_wide <- pred_pam |> select(tend
-                                  , complications
-                                  , prob_pam) |>
-    pivot_wider(names_from = complications
-                , values_from = prob_pam) |>
-    mutate(diff = abs(yes - no))
+# transform into wide format for plotting area between survival curves
+pred_pam_wide <- pred_pam |> select(tend
+                                , complications
+                                , prob_pam) |>
+  pivot_wider(names_from = complications
+              , values_from = prob_pam) |>
+  mutate(diff = abs(yes - no))
 
-  # plot survival curves and area corresponding to rmst
-  gg_rmst_pam <- ggplot(pred_pam_wide, aes(x = tend)) +
-    geom_line(aes(y = yes), col="firebrick2") +
-    geom_line(aes(y = no), col="steelblue") +
-    # Ribbon for the shaded area
-    geom_ribbon(aes(ymin = yes, ymax = no), fill = "grey", alpha = 0.2) +
-    labs(y = "Survival Probability", x = "Time",
-         title = "RMST complications 'yes' and 'no'",
-         subtitle = "Grey area represents RMST difference") +
-    theme_minimal() +
-    theme(legend.position = "none")
+# plot survival curves and area corresponding to rmst
+gg_rmst_pam <- ggplot(pred_pam_wide, aes(x = tend)) +
+  geom_line(aes(y = yes), col="firebrick2") +
+  geom_line(aes(y = no), col="steelblue") +
+  # Ribbon for the shaded area
+  geom_ribbon(aes(ymin = yes, ymax = no), fill = "grey", alpha = 0.2) +
+  labs(y = "Survival Probability", x = "Time",
+        title = "RMST complications 'yes' and 'no'",
+        subtitle = "Grey area represents RMST difference") +
+  theme_minimal() +
+  theme(legend.position = "none")
 
-  ## DT
-  # calculate rmst (integrate step function)
-  rmst_dt <- pred_dt %>%
-    group_by(complications) %>%
-    summarise(rmst = sum(prob_dt * diff(c(0, tend))))
+## DT
+# calculate rmst (integrate step function)
+rmst_dt <- pred_dt %>%
+  group_by(complications) %>%
+  summarise(rmst = sum(prob_dt * diff(c(0, tend))))
 
-  rmst_dt
+rmst_dt
 
-  # transform into wide format for plotting area between survival curves
-  pred_dt_wide <- pred_dt |> select(tend
-                                      , complications
-                                      , prob_dt) |>
-    pivot_wider(names_from = complications
-                , values_from = prob_dt) |>
-    mutate(diff = abs(yes - no))
+# transform into wide format for plotting area between survival curves
+pred_dt_wide <- pred_dt |> select(tend
+                                    , complications
+                                    , prob_dt) |>
+  pivot_wider(names_from = complications
+              , values_from = prob_dt) |>
+  mutate(diff = abs(yes - no))
 
-  # plot survival curves and area corresponding to rmst
-  gg_rmst_dt <- ggplot(pred_dt_wide, aes(x = tend)) +
-    geom_line(aes(y = yes), col="firebrick2") +
-    geom_line(aes(y = no), col="steelblue") +
-    # Ribbon for the shaded area
-    geom_ribbon(aes(ymin = yes, ymax = no), fill = "grey", alpha = 0.2) +
-    labs(y = "Survival Probability", x = "Time",
-         title = "RMST complications 'yes' and 'no'",
-         subtitle = "Grey area represents RMST difference") +
-    theme_minimal() +
-    theme(legend.position = "none")
+# plot survival curves and area corresponding to rmst
+gg_rmst_dt <- ggplot(pred_dt_wide, aes(x = tend)) +
+  geom_line(aes(y = yes), col="firebrick2") +
+  geom_line(aes(y = no), col="steelblue") +
+  # Ribbon for the shaded area
+  geom_ribbon(aes(ymin = yes, ymax = no), fill = "grey", alpha = 0.2) +
+  labs(y = "Survival Probability", x = "Time",
+        title = "RMST complications 'yes' and 'no'",
+        subtitle = "Grey area represents RMST difference") +
+  theme_minimal() +
+  theme(legend.position = "none")
 
 ## merge results
 tbd
