ef002871c10b10e9f7b21020ac198e7f03964b10
Piller <Johannes.Piller@lmu.de>
2025-07-15 13:14:11 +0200

    tidy repo

---
+++ code/notused/ex-reductions-cr.R
@@ -0,0 +1,223 @@
+## Reduction techniques competing risk
+
+# load libraries
+library(dplyr)
+library(tidyr)
+library(mgcv)
+library(pammtools)
+library(xgboost)
+library(ggplot2)
+library(mvna)              # pneunomia data set
+library(etm)               # aalen johansen estimator
+library(cmprsk)            # needed for competing risks in etm, cf. Beyersmann p. 79
+library(pseudo)
+library(mlr3)
+library(mlr3learners)
+library(mlr3pipelines)
+library(mlr3proba)
+theme_set(theme_bw())
+
+setwd("C:/Users/ra63liw/Documents/98_git/reduction-techniques")
+source("code/functions/etm-ci-trafo.R")
+source("code/functions/get-cif-from-haz-df.R")
+
+
+
+## initialize variables for plotting
+# fontsize
+label_size = 24
+headline_size = 24
+
+# initialize lines and dots
+linewidth = 1
+pointsize = 2
+strokewidth = 1.5
+# model_colors <- c("pam" = "firebrick2", "dt" = "steelblue", "pv" = "springgreen4", "aj" = "black")
+model_colors <- c(
+  "pam" = "#D55E00",   # vivid reddish-orange
+  "dt"  = "#0072B2",   # deep sky blue
+  "pv"  = "#009E73",   # bluish green (unchanged)
+  "aj"  = "#000000"    # black (unchanged)
+)
+
+model_fills  <- rep("darkgrey", 4); names(model_fills) <- names(model_colors)
+
+# theme
+# theme_cif <- theme_minimal(base_size = label_size) +
+#   theme(
+#     strip.text   = element_text(size = headline_size, face = "bold"),
+#     axis.title   = element_text(size = label_size),
+#     axis.text    = element_text(size = label_size),
+#     legend.text  = element_text(size = label_size),
+#     legend.position = "right",
+#     panel.spacing.x = unit(2, "lines")
+#   )
+
+theme_cif <- theme(
+  text = element_text(size = 10),
+  axis.title = element_text(size = 11),
+  axis.text = element_text(size = 9),
+  legend.position = "bottom",
+  legend.title = element_text(size = 10),
+  legend.text = element_text(size = 9),
+  strip.text = element_text(size = 11, face = "bold")
+)
+
+# load pneunomia data
+data(sir.adm, package = "mvna")
+head(sir.adm)
+
+## -------------------------------------------------------------------------- ##
+## Aalen-Johansen, following Beyersmann chapter 4.3
+## -------------------------------------------------------------------------- ##
+tra <- matrix(FALSE, ncol = 3, nrow = 3)
+dimnames(tra) <- list(c("0", "1", "2"), c("0", "1", "2"))
+tra[1, 2:3] <- TRUE
+tra
+
+to <- ifelse(sir.adm$status==0
+             , "cens"
+             , ifelse(sir.adm$status==1,2,1)
+)
+my.sir.data <- data.frame(id=sir.adm$id
+                          , from=0
+                          , to
+                          , time=sir.adm$time
+                          , pneu=sir.adm$pneu
+)
+
+my.nelaal.nop <- mvna(my.sir.data[my.sir.data$pneu == 0, ]
+                      , c("0", "1", "2"), tra, "cens")
+my.nelaal.p <- mvna(my.sir.data[my.sir.data$pneu == 1, ]
+                    , c("0", "1", "2"), tra, "cens")
+
+my.sir.cif <- cuminc(my.sir.data$time
+                     , my.sir.data$to
+                     , group=my.sir.data$pneu
+                     , cencode="cens")
+
+my.sir.etm.nop <- etm(my.sir.data[my.sir.data$pneu == 0, ], c("0", "1", "2"), tra, "cens", s = 0)
+my.sir.etm.p <- etm(my.sir.data[my.sir.data$pneu == 1, ], c("0", "1", "2"), tra, "cens", s = 0)
+
+# use internal etm function to generate aj with confidence interval
+ls_aj_out <- list(nop = list(ci.transfo(my.sir.etm.nop, tr.choice = '0 1', transfo = "cloglog")
+                             , ci.transfo(my.sir.etm.nop, tr.choice = '0 2', transfo = "cloglog"))
+                  , p = list(ci.transfo(my.sir.etm.p, tr.choice = '0 1', transfo = "cloglog")
+                             , ci.transfo(my.sir.etm.p, tr.choice = '0 2', transfo = "cloglog"))
+)
+
+# build data frame for plotting
+ndf_cr_aj <- rbind(
+  data.frame(tend = ls_aj_out$p[[1]]$`0 1`$time
+             , cif = ls_aj_out$p[[1]]$`0 1`$P
+             , cif_lower = ls_aj_out$p[[1]]$`0 1`$lower
+             , cif_upper = ls_aj_out$p[[1]]$`0 1`$upper
+             , cause = 1
+             , pneu = 0),
+  data.frame(tend = ls_aj_out$p[[2]]$`0 2`$time
+             , cif = ls_aj_out$p[[2]]$`0 2`$P
+             , cif_lower = ls_aj_out$p[[2]]$`0 2`$lower
+             , cif_upper = ls_aj_out$p[[2]]$`0 2`$upper
+             , cause = 2
+             , pneu = 0),
+  data.frame(tend = ls_aj_out$nop[[1]]$`0 1`$time
+             , cif = ls_aj_out$nop[[1]]$`0 1`$P
+             , cif_lower = ls_aj_out$nop[[1]]$`0 1`$lower
+             , cif_upper = ls_aj_out$nop[[1]]$`0 1`$upper
+             , cause = 1
+             , pneu = 1),
+  data.frame(tend = ls_aj_out$nop[[2]]$`0 2`$time
+             , cif = ls_aj_out$nop[[2]]$`0 2`$P
+             , cif_lower = ls_aj_out$nop[[2]]$`0 2`$lower
+             , cif_upper = ls_aj_out$nop[[2]]$`0 2`$upper
+             , cause = 2
+             , pneu = 1)
+) |>
+  mutate(
+    cause = factor(cause, labels = c("Death", "Discharge"))
+    , pneu = factor(pneu, labels = c("Pneumonia", "No Pneumonia"))
+    , model = "aj"
+  )
+
+## -------------------------------------------------------------------------- ##
+## PAM, ndf_cr can be used for DT as well
+## -------------------------------------------------------------------------- ##
+
+# create ped data set
+ped_cr <- as_ped(sir.adm, Surv(time, status)~ ., combine = TRUE) |>
+  mutate(cause = as.factor(cause), pneu = as.factor(pneu))
+
+# estimate pam
+pam <- pamm(ped_status ~ s(tend, by = interaction(cause, pneu)) + cause*pneu, data = ped_cr)
+
+# pem xgb 
+lrn_xgb_pem = po("encode", method = "treatment") %>>% 
+    lrn(
+        'regr.xgboost', 
+        nrounds = 1000, 
+        eta = 0.12, 
+        max_depth = 3, 
+        base_score = 1, # very important for poisson loss
+        objective = "count:poisson", 
+        lambda = 0) |> 
+    as_learner()
+
+tsk_pneu = TaskRegr$new(
+    id = "pneu", 
+    backend = select(ped_cr, ped_status, tend, cause, pneu), 
+    target = "ped_status")
+lrn_xgb_pem$train(tsk_pneu)
+
+
+# build new data frame including cif for pam
+ndf_cr <- ped_cr |>
+  make_newdata(tend = unique(tend), pneu = unique(pneu), cause = unique(cause))
+
+
+haz_xgb_pem = lrn_xgb_pem$predict_newdata(ndf_cr) |> as.data.table()
+ndf_cr_xgb_pem <- ndf_cr |> cbind(haz_xgb_pem = haz_xgb_pem[["response"]]) |> 
+  group_by(pneu, cause) |>
+  add_cumu_hazard(pam) |>
+  mutate(cumu_haz_xgb_pem = cumsum(haz_xgb_pem*intlen))
+
+ndf_cr_xgb_pem_long <- ndf_cr |> 
+  cbind(haz_xgb_pem = haz_xgb_pem[["response"]]) |> 
+  group_by(pneu, cause) |>
+  add_cumu_hazard(pam) |> 
+  mutate(cumu_haz_xgb_pem = cumsum(haz_xgb_pem * intlen)) |> 
+  ungroup() |> 
+  select(-haz_xgb_pem) |> 
+  pivot_longer(
+    cols = c(cumu_hazard, cumu_haz_xgb_pem),
+    names_to = "method",
+    values_to = "cumu_hazard"
+  ) |> 
+  mutate(
+    method = recode(method,
+                    cumu_hazard = "PAM",
+                    cumu_haz_xgb_pem = "XGBOOST")
+  )
+
+# compare hazards
+ggplot(ndf_cr_xgb_pem_long, aes(x=tend, lty= pneu, col = method)) + 
+  geom_line(aes(y=cumu_hazard)) + 
+  facet_wrap(~cause)
+
+source("code/functions/get-cif-from-haz-df.R")
+
+ndf_cr_xgb_pem <- ndf_cr_xgb_pem |>
+  mutate(transition = ifelse(cause == 1, "1->2", "1->3")) |>
+  group_by(pneu, cause) |> 
+  mutate(cumu_hazard = cumsum(haz_xgb_pem)) |>
+  add_trans_prob(pam, ci = FALSE) |> # object not used because ci = FaLSE and cumu_hazard in data set
+  ungroup()
+
+ndf_cr_xgb_pem <- ndf_cr_xgb_pem |> 
+  group_by(pneu) |> 
+  arrange(pneu, tend) |> 
+  add_cif_from_haz(haz_col = "haz_xgb_pem")
+
+ggplot(ndf_cr_xgb_pem, aes(x=tend, lty= pneu)) + 
+  # geom_line(aes(y=trans_prob)) + 
+  geom_line(aes(y=cif)) + 
+  facet_wrap(~cause)
---
+++ code/notused/ex-reductions-tumor.R
@@ -0,0 +1,130 @@
+library(mlr3)
+library(mlr3learners)
+library(mlr3extralearners)
+library(mlr3pipelines)
+library(mlr3proba)
+library(survival)
+library(pammtools)
+library(dplyr)
+library(ggplot2)
+theme_set(theme_bw())
+
+# Load data
+data("tumor", package = "pammtools")
+head(tumor)
+
+# Create survival task
+tsk_tumor = TaskSurv$new(
+    id = "tumor", 
+    backend = select(tumor, days, status, complications), 
+    time = "days", 
+    event = "status")
+
+# Define cut points for discrete time models
+cut = seq(0, max(tumor$days), length.out = 100) |> round()
+
+# ============================================================================
+# 1. KAPLAN-MEIER ESTIMATES (STRATIFIED BY COMPLICATIONS)
+# ============================================================================
+
+# Fit Kaplan-Meier for each group
+km_complications = survival::survfit(Surv(days, status) ~ complications, data = tumor)
+km_tidy = broom::tidy(km_complications)
+
+# ============================================================================
+# 2. DISCRETE TIME PIPELINE WITH RANDOM FOREST
+# ============================================================================
+
+# Define discrete time pipeline with random forest
+pipeline_rf = ppl(
+  "survtoclassif_disctime",
+  learner = lrn("classif.ranger", num.trees = 1000L),
+  cut = cut,
+  graph_learner = TRUE)
+
+# Train the pipeline
+pipeline_rf$train(tsk_tumor)
+
+# Get predictions
+pred_rf = pipeline_rf$predict(tsk_tumor)
+
+# Extract survival curves for each group
+rf_no_complications = pred_rf$data$distr[1, ]
+rf_yes_complications = pred_rf$data$distr[2, ]
+
+# ============================================================================
+# 3. PIECEWISE EXPONENTIAL MODEL (PEM) PIPELINE WITH XGBOOST
+# ============================================================================
+
+# Define PEM pipeline with XGBoost
+pipeline_pem_xgb = po("encode", method = "treatment") %>>% 
+    ppl(
+     "survtoregr_pem",
+    learner = lrn("regr.xgboost", 
+                nrounds = 1000, 
+                eta = 0.12, 
+                max_depth = 2, 
+                base_score = 1, # very important for poisson loss
+                objective = "count:poisson", 
+                lambda = 0),
+  cut = cut,
+  graph_learner = TRUE) |> as_learner()
+
+pipeline_pem_xgb$train(tsk_tumor)
+pred_xgb_PEM = pipeline_pem_xgb$predict(tsk_tumor)
+
+# Get predictions
+# Extract survival curves for each group
+pem_xgb_no_complications = pred_xgb_PEM$data$distr[1, ]
+pem_xgb_yes_complications = pred_xgb_PEM$data$distr[2, ]
+
+# ============================================================================
+# CREATE COMPARISON PLOT
+# ============================================================================
+
+# Create data frame for plotting
+plot_data = data.frame(
+  time = as.numeric(names(rf_no_complications)),
+  # Random Forest predictions
+  rf_no = rf_no_complications,
+  rf_yes = rf_yes_complications,
+  # PEM XGBoost predictions  
+  pem_xgb_no = pem_xgb_no_complications,
+  pem_xgb_yes = pem_xgb_yes_complications
+)
+
+# Create the comparison plot
+p_comparison = ggplot() +
+  geom_surv(data = km_tidy, 
+             aes(x = time, y = estimate, col = "Kaplan-Meier", lty = strata)) +
+  geom_surv(data = plot_data, 
+             aes(x = time, y = rf_no, col = "Discrete Time RF", lty = "no")) +
+  geom_surv(data = plot_data, 
+             aes(x = time, y = rf_yes, col = "Discrete Time RF", lty = "yes")) +
+  geom_surv(data = plot_data, 
+             aes(x = time, y = pem_xgb_no, col = "PEM XGBoost", lty = "no")) +
+  geom_surv(data = plot_data, 
+             aes(x = time, y = pem_xgb_yes, col = "PEM XGBoost", lty = "yes")) +
+  ylim(c(0, 1)) +
+  labs(y = "Survival Probability", 
+       x = "Time (days)") +
+  scale_color_discrete(name = "Method") +
+  scale_linetype_discrete(name = "Complications") +
+  theme(legend.position = "bottom")
+
+# Display the plot
+p_comparison
+
+
+
+
+
+
+
+
+
+
+
+
+
+
